51eda0d6256a993619d323d4c845dcc0dc5ff165
typesdk_code <support@typesdk.com>
2017-01-09 12:01:23 +0800

    更新阿里游戏聚合sdk

---
+++ SdkServer_free_git/init_redis.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+#redis连接配置
+RedisIP='127.0.0.1'
+RedisPort='6379'
+RedisPWD='typesdk.com'
+
+#热加载版本号，如修改相关参数可更改这个版本号,server会重新reload相关配置。手动重启server进程也可以
+ver='1'
+
+#sdkserver与gameserver通讯的密钥(必须和游戏服务器相匹配)
+apikey='typesdk.com'
+
+#游戏ID号和游戏名称
+gameid='1000'
+gamename='乔乔大冒险'
+
+#初始化redis库
+redis-cli -h $RedisIP -p $RedisPort -a $RedisPWD hset GAME:$gameid:ID id $gameid
+redis-cli -h $RedisIP -p $RedisPort -a $RedisPWD hset GAME:$gameid:ID gameName $gamename
+redis-cli -h $RedisIP -p $RedisPort -a $RedisPWD hset GAME:$gameid:ID apikey $apikey
+redis-cli -h $RedisIP -p $RedisPort -a $RedisPWD hset GAME:$gamename:NAME id $gameid
+redis-cli -h $RedisIP -p $RedisPort -a $RedisPWD hset GAME:$gamename:NAME name $gamename
+redis-cli -h $RedisIP -p $RedisPort -a $RedisPWD hset GAME:$gameid:VERSION version $ver
+
+#渠道号(必须和sdk相匹配,sdk字典的常量)和渠道相关参数
+fielduc='ch301'
+#阿里游戏渠道参数请修改gameId=app_id,apiKey=app_key，sdk_cp_id=cpId，其他参数不用修改
+valueuc='{"id":"1","name":"AliUC","attrs":{"app_id":"123345676","app_key":"1234567","channel_id":"1","sdk_cp_id":"123456","sdk_name":"AliUC","itemLists":[]}}'
+redis-cli -h $RedisIP -p $RedisPort -a $RedisPWD hset GAME:$gameid:CHANNEL $fielduc $valueuc
+
+field360='ch3'
+#360渠道参数请修改appId=app_id,appKey=app_key，appSecret=secret_key,private_key算法请参考渠道sdk文档，其他参数不用修改
+value360='{"id":"3","name":"360","attrs":{"app_id":"123345676","app_key":"123345676","channel_id":"3","private_key":"123345676","sdk_name":"360","secret_key":"123345676","itemLists":[]}}'
+redis-cli -h $RedisIP -p $RedisPort -a $RedisPWD hset GAME:$gameid:CHANNEL $field360 $value360
+
+fieldbaidu='ch5'
+#百度渠道参数请修改AppID=app_id，ProductKey=app_key，ProductSecret=secret_key，其他参数不用修改
+valuebaidu='{"id":"5","name":"Baidu","attrs":{"app_id":"123345676","app_key":"123345676","channel_id":"5","sdk_name":"Baidu","secret_key":"123345676","itemLists":[]}}'
+redis-cli -h $RedisIP -p $RedisPort -a $RedisPWD hset GAME:$gameid:CHANNEL $fieldbaidu $valuebaidu
+
+fieldyouku='ch6'
+#优酷渠道参数请修改AppID=app_id，AppKey=app_key，AppSecret=secret_key，PayKey=private_key，pay_call_back_url=支付回调地址（http://ip:port/$gameid/6/Pay）其他参数不用修改
+valueyouku='{"id":"6","name":"YouKu","attrs":{"app_id":"123345676","app_key":"123345676","channel_id":"6","pay_call_back_url":"http://120.27.201.22:40000/1001/6/Pay","private_key":"123345676","sdk_name":"YouKu","secret_key":"123345676","itemLists":[]}}'
+redis-cli -h $RedisIP -p $RedisPort -a $RedisPWD hset GAME:$gameid:CHANNEL $fieldyouku $valueyouku
+
+fieldxiaomi='ch7'
+#小米渠道参数请修改appId=app_id，appkey=app_key，appsecret=secret_key，其他参数不用修改
+valuexiaomi='{"id":"7","name":"XiaoMi","attrs":{"app_id":"123345676","app_key":"123345676","channel_id":"7","sdk_name":"XiaoMi","secret_key":"123345676","itemLists":[]}}'
+redis-cli -h $RedisIP -p $RedisPort -a $RedisPWD hset GAME:$gameid:CHANNEL $fieldxiaomi $valuexiaomi
+
+fieldhuawei='ch9'
+#华为渠道参数请修改appId=app_id，appkey=app_key，pubPayKey=product_key，priPayKey=secret_key，pubLoginKey=PublicKey，CPID=CPID,其他参数不用修改
+valuehuawei='{"id":"9","name":"HuaWei","attrs":{"app_id":"123345676","app_key":"123345676","channel_id":"9","CPID":"123345676","product_key":"123345676","PublicKey":"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAmKLBMs2vXosqSR2rojMzioTRVt8oc1ox2uKjyZt6bHUK0u+OpantyFYwF3w1d0U3mCF6rGUnEADzXiX/2/RgLQDEXRD22er31ep3yevtL/r0qcO8GMDzy3RJexdLB6z20voNM551yhKhB18qyFesiPhcPKBQM5dnAOdZLSaLYHzQkQKANy9fYFJlLDo11I3AxefCBuoG+g7ilti5qgpbkm6rK2lLGWOeJMrF+Hu+cxd9H2y3cXWXxkwWM1OZZTgTq3Frlsv1fgkrByJotDpRe8SwkiVuRycR0AHsFfIsuZCFwZML16EGnHqm2jLJXMKIBgkZTzL8Z+201RmOheV4AQIDAQAB","sdk_name":"HuaWei","secret_key":"123345676","itemLists":[]}}'
+redis-cli -h $RedisIP -p $RedisPort -a $RedisPWD hset GAME:$gameid:CHANNEL $fieldhuawei $valuehuawei
+
+fieldoppo='ch10'
+#oppo渠道参数请修改appId=app_id，appkey=app_key，appsecret=secret_key，其他参数不用修改
+valueoppo='{"id":"10","name":"Oppo","attrs":{"app_id":"123345676","app_key":"123345676","channel_id":"10","sdk_name":"Oppo","secret_key":"123345676","itemLists":[]}}'
+redis-cli -h $RedisIP -p $RedisPort -a $RedisPWD hset GAME:$gameid:CHANNEL $fieldoppo $valueoppo
+
+fieldvivo='ch14'
+#vivo渠道参数请修改AppID=app_id，cpkey=app_key，cpid=sdk_cp_id，PayKey=private_key，pay_call_back_url=支付回调地址（http://ip:port/$gameid/14/Pay）其他参数不用修改
+valuevivo='{"id":"14","name":"Vivo","attrs":{"app_id":"123345676","app_key":"123345676","channel_id":"14","pay_call_back_url":"http://120.27.201.22:40000/1001/14/Pay","sdk_cp_id":"123345676","sdk_name":"Vivo","itemLists":[]}}'
+redis-cli -h $RedisIP -p $RedisPort -a $RedisPWD hset GAME:$gameid:CHANNEL $fieldvivo $valuevivo
+
+fieldwandoujia='ch54'
+#海马玩渠道参数请修改appId=app_id，appkey=app_key，其他参数不用修改
+valuewandoujia='{"id":"54","name":"HaiMa","attrs":{"app_id":"1234567","app_key":"1234567","channel_id":"54","sdk_name":"HaiMa","itemLists":[]}}'
+redis-cli -h $RedisIP -p $RedisPort -a $RedisPWD hset GAME:$gameid:CHANNEL $fieldwandoujia $valuewandoujia
+
+fielddangle='ch47'
+#当乐渠道参数请修改app=app_id，appKey=app_key，merchantId=product_id，payKey=secret_key其他参数不用修改
+valuedangle='{"id":"47","name":"DangLe","attrs":{"app_id":"123345676","app_key":"123345676","channel_id":"47","product_id":"123345676","sdk_name":"DangLe","secret_key":"123345676","itemLists":[]}}'
+redis-cli -h $RedisIP -p $RedisPort -a $RedisPWD hset GAME:$gameid:CHANNEL $fielddangle $valuedangle
---
+++ SdkServer_free_git/install_allinone.sh
@@ -0,0 +1,194 @@
+#!/bin/sh
+setenforce 0
+echo -e "\033[31m ###################################################################################################### \033[0m"
+echo -e "\033[32m ################################### install typesdkserver allinone ################################### \033[0m"
+echo -e "\033[32m ################################### website:http://www.typesdk.com ################################### \033[0m"
+echo -e "\033[32m ################################### QQ:1771930259                  ################################### \033[0m"
+echo -e "\033[31m ###################################################################################################### \033[0m"
+
+myPath="/data/"
+if [ ! -d "$myPath" ]; then  
+	mkdir -p "$myPath"
+fi 
+mkdir  -p /data/typesdk_server
+cd ..
+mv SdkServer_free_git/ Tools /data/typesdk_server/
+echo -e "\033[31m ###################################################################################################### \033[0m"
+echo -e "\033[32m ##################################### install and start mysql  ####################################### \033[0m"
+echo -e "\033[31m ###################################################################################################### \033[0m"
+cd /data/typesdk_server/Tools
+rpm -Uvh mysql57-community-release-el6-9.noarch.rpm 
+yes|cp mysql-community.repo /etc/yum.repos.d/mysql-community.repo  
+yum install mysql-server -y 
+mkdir -p /data/typesdk_server/mysql_data
+chown -R mysql.mysql /data/typesdk_server/mysql_data
+cp /etc/my.cnf /etc/my.cnf.bak
+>/etc/my.cnf
+cat > /etc/my.cnf <<EOF
+[mysqld]
+datadir=/data/typesdk_server/mysql_data
+socket=/var/lib/mysql/mysql.sock
+symbolic-links=0
+log-error=/var/log/mysqld.log
+pid-file=/var/run/mysqld/mysqld.pid
+
+EOF
+service mysqld start
+mysql < /data/typesdk_server/Tools/typesdk_server.sql
+mysqladmin -u root password "typesdk.com"
+
+
+echo -e "\033[31m ###################################################################################################### \033[0m"
+echo -e "\033[32m ##################################### install and start nginx  ####################################### \033[0m"
+echo -e "\033[31m ###################################################################################################### \033[0m"
+cd /data/typesdk_server/Tools
+rpm -Uvh nginx-1.8.1-1.el6.ngx.x86_64.rpm  
+mkdir -p /data/typesdk_server/nginx_html/config
+cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
+>/etc/nginx/nginx.conf
+cat > /etc/nginx/nginx.conf <<EOF
+#user  nobody;
+worker_processes  auto;
+events {
+    worker_connections  102400;
+}
+http {
+    include       mime.types;
+    default_type  application/octet-stream;
+    sendfile        on;
+    keepalive_timeout  65;
+    gzip  on;
+    server {
+        listen       80;
+        #server_name  your.domain.name;
+        location /config {
+            root   /data/typesdk_server/nginx_html;
+            index  index.html index.htm;
+        }
+        location / {
+            proxy_pass http://127.0.0.1:40000;
+        }
+        error_page   500 502 503 504  /50x.html;
+        location = /50x.html {
+            root   html;
+        }
+    }
+}
+
+EOF
+
+touch /data/typesdk_server/nginx_html/config/config.txt
+cat > /data/typesdk_server/nginx_html/config/config.txt <<EOF
+{
+    "profile_verison": "2.0.1",
+    "itemListUrl": "http://127.0.0.1:40000/getChannelConfig",
+    "other": {
+        "pay_mode": "release",
+        "pushservice": "1",
+        "open_log": "1",
+        "crash_url": "127.0.0.1",
+        "crash_port": "21",
+        "user_name": "crash",
+        "user_pass_word": "crashlog"
+    },
+    "crash_profile": {
+        "collect_model_regular": "all",
+        "collect_sdk_regular": "all",
+        "catch_tag": [
+            "Unity"
+        ],
+        "is_open": "1"
+    },
+    "payment_profile": {
+        "sdk_open_regular": "all",
+        "sdk_open_list": [],
+        "is_appstorepay_open": "1"
+    },
+    "white_id": {
+        "getip": "http://127.0.0.1:30003/getIp"        
+    }
+}
+
+EOF
+service nginx start
+
+echo -e "\033[31m ###################################################################################################### \033[0m"
+echo -e "\033[32m ##################################### install and start redis  ####################################### \033[0m"
+echo -e "\033[31m ###################################################################################################### \033[0m"
+cd /data/typesdk_server/Tools
+rpm -Uvh jemalloc-3.6.0-1.el6.x86_64.rpm  
+rpm -Uvh redis-3.2.4-1.el6.remi.x86_64.rpm  
+mkdir -p /data/typesdk_server/redis_data
+cp /etc/redis.conf /etc/redis.conf.bak
+cat > /etc/redis.conf << EOF
+bind 127.0.0.1
+protected-mode yes
+port 6379
+tcp-backlog 511
+timeout 0
+tcp-keepalive 300
+daemonize no
+supervised no
+pidfile /var/run/redis_6379.pid
+loglevel notice
+logfile /var/log/redis/redis.log
+databases 16
+save 900 1
+save 300 10
+save 60 10000
+stop-writes-on-bgsave-error yes
+rdbcompression yes
+rdbchecksum yes
+dbfilename sdk.rdb
+dir /data/typesdk_server/redis_data  
+slave-serve-stale-data yes
+slave-read-only yes
+requirepass typesdk.com
+repl-diskless-sync no
+repl-diskless-sync-delay 5
+repl-disable-tcp-nodelay no
+slave-priority 100
+appendonly no
+appendfilename "appendonly.aof"
+appendfsync everysec
+no-appendfsync-on-rewrite no
+auto-aof-rewrite-percentage 100
+auto-aof-rewrite-min-size 64mb
+aof-load-truncated yes
+lua-time-limit 5000
+slowlog-log-slower-than 10000
+slowlog-max-len 128
+latency-monitor-threshold 0
+notify-keyspace-events ""
+hash-max-ziplist-entries 512
+hash-max-ziplist-value 64
+list-max-ziplist-size -2
+list-compress-depth 0
+set-max-intset-entries 512
+zset-max-ziplist-entries 128
+zset-max-ziplist-value 64
+hll-sparse-max-bytes 3000
+activerehashing yes
+client-output-buffer-limit normal 0 0 0
+client-output-buffer-limit slave 256mb 64mb 60
+client-output-buffer-limit pubsub 32mb 8mb 60
+hz 10
+aof-rewrite-incremental-fsync yes
+
+EOF
+
+service redis start
+sh /data/typesdk_server/Tools/init_redis.sh
+
+
+echo -e "\033[31m ###################################################################################################### \033[0m"
+echo -e "\033[32m #####################################       install node       ####################################### \033[0m"
+echo -e "\033[31m ###################################################################################################### \033[0m"
+cd /data/typesdk_server/Tools
+rpm -Uvh nodejs-6.8.1-1nodesource.el6.x86_64.rpm
+npm config set registry http://registry.cnpmjs.org
+chmod +x /data/typesdk_server/SdkServer_free_git/node_modules/pm2/bin/pm2
+ln -s /data/typesdk_server/SdkServer_free_git/node_modules/pm2/bin/pm2 /usr/bin/pm2
+echo -e "\033[32m #####################################     staring server  ####################################### \033[0m"
+cd /data/typesdk_server/SdkServer_free_git/bin
+pm2 start www
